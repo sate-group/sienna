b23554718807d871a8e796b36562d5d7957e29ef
welcomehyunseo <welcomehyunseo@gmail.com>
2022-06-27 07:02:48 +0900

    remove package

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/sate-infra/sienna
 
 go 1.18
-
-require github.com/google/uuid v1.3.0 // indirect
---
+++ server_options.go
@@ -0,0 +1,16 @@
+package sienna
+
+const (
+	DEFULAT_PORT = uint16(4000)
+)
+
+type ServerOptions struct {
+	Port uint16
+}
+
+func (opts *ServerOptions) getPort() uint16 {
+	if opts != nil && opts.Port != 0 {
+		return opts.Port
+	}
+	return DEFULAT_PORT
+}
